9608865c852e8d50709f9842d29b08994081cb64
Yuwnei Xiao <xiaoluoboding@gmail.com>
2023-11-03 11:45:25 +0800

    feat: make promise return promise

--- packages/state.ts
+++ packages/state.ts
@@ -92,21 +92,15 @@ class Observer {
   }
 
   message = (message: string | Component, data?: ExternalToast) => {
-    const id = data?.id || toastsCounter++
-    this.publish({ ...data, id, title: message })
-    return id
+    return this.create({ ...data, message })
   }
 
   error = (message: string | Component, data?: ExternalToast) => {
-    const id = data?.id || toastsCounter++
-    this.publish({ ...data, id, type: 'error', title: message })
-    return id
+    return this.create({ ...data, type: 'error', message })
   }
 
   success = (message: string | Component, data?: ExternalToast) => {
-    const id = data?.id || toastsCounter++
-    this.publish({ ...data, id, type: 'success', title: message })
-    return id
+    return this.create({ ...data, type: 'success', message })
   }
 
   info = (message: string | Component, data?: ExternalToast) => {
@@ -121,9 +115,57 @@ class Observer {
     return this.create({ ...data, type: 'loading', message })
   }
 
-  promise = (promise: PromiseT, data?: PromiseData) => {
-    const id = data?.id || toastsCounter++
-    this.publish({ ...data, promise, id })
+  promise = <ToastData>(
+    promise: PromiseT<ToastData>,
+    data?: PromiseData<ToastData>
+  ) => {
+    if (!data) {
+      // Nothing to show
+      return
+    }
+
+    let id: string | number | undefined = undefined
+    if (data.loading !== undefined) {
+      id = this.create({
+        ...data,
+        promise,
+        type: 'loading',
+        message: data.loading
+      })
+    }
+
+    const p = promise instanceof Promise ? promise : promise()
+
+    let shouldDismiss = id !== undefined
+
+    p.then((promiseData) => {
+      if (data.success !== undefined) {
+        shouldDismiss = false
+        const message =
+          typeof data.success === 'function'
+            ? data.success(promiseData)
+            : data.success
+        this.create({ id, type: 'success', message })
+      }
+    })
+      .catch((error) => {
+        if (data.error !== undefined) {
+          shouldDismiss = false
+          const message =
+            typeof data.error === 'function' ? data.error(error) : data.error
+          this.create({ id, type: 'error', message })
+        }
+      })
+      .finally(() => {
+        if (shouldDismiss) {
+          // Toast is still in load state (and will be indefinitely — dismiss it)
+          this.dismiss(id)
+          id = undefined
+        }
+
+        data.finally?.()
+      })
+
     return id
   }
 
