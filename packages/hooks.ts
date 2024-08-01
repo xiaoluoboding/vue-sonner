da333544fd2a842769e2d423bc2eb193e7764e6a
sadeghbarati <sadeghbaratiwork@gmail.com>
2024-08-01 12:48:47 +0330

    feat: add `useVueSonner` and export some types

--- packages/hooks.ts
+++ packages/hooks.ts
@@ -1,4 +1,6 @@
 import { ref, watchEffect } from 'vue'
+import type { ToastT } from './types'
+import { ToastState } from './state'
 
 export function useIsDocumentHidden() {
   const isDocumentHidden = ref(false)
@@ -13,5 +15,35 @@ export function useIsDocumentHidden() {
 
   return {
     isDocumentHidden,
+  }
+}
+
+export function useVueSonner() {
+  const activeToasts = ref<ToastT[]>([])
+
+  watchEffect((onInvalidate) => {
+    const unsubscribe = ToastState.subscribe((toast) => {
+      if ('dismiss' in toast && toast.dismiss) {
+        return activeToasts.value.filter((t) => t.id !== toast.id);
+      }
+
+      const existingToastIndex = activeToasts.value.findIndex((t) => t.id === toast.id);
+      if (existingToastIndex !== -1) {
+        const updatedToasts = [...activeToasts.value];
+        updatedToasts[existingToastIndex] = { ...updatedToasts[existingToastIndex], ...toast };
+
+        activeToasts.value = updatedToasts
+      } else {
+        activeToasts.value = [toast, ...activeToasts.value]
+      }
+    })
+
+    onInvalidate(() => {
+      unsubscribe()
+    })
+  })
+
+  return {
+    activeToasts
   }
 }
