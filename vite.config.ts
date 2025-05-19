1834baba709ec34ace4f2f3253afdc8473af8952
mod08 <maege.daetwyler@hispeed.ch>
2025-05-19 10:46:08 +0200

    feat: add option to import css manually

--- vite.config.ts
+++ vite.config.ts
@@ -35,25 +35,35 @@ export default defineConfig(({ command, mode }) => {
   if (mode === 'lib') {
     userConfig.build = {
       lib: {
-        entry: resolve(__dirname, 'src/packages/index.ts'),
-        name: 'VueSonner',
-        fileName: 'vue-sonner'
+        entry: {
+          'vue-sonner': resolve(__dirname, 'src/packages/index.ts'),
+          'manual-css': resolve(__dirname, 'src/packages/index.ts')
+        },
+        fileName: (format, entryName) => {
+          if (entryName === 'vue-sonner') {
+            return `vue-sonner.${format === 'es' ? 'js' : format}`
+          }
+          if (entryName === 'manual-css') {
+            return `vue-sonner.manual.${format === 'es' ? 'js' : format}`
+          }
+        },
+        formats: ['es', 'cjs']
       },
       outDir: 'lib',
       emptyOutDir: true,
-      cssCodeSplit: false,
+      cssCodeSplit: true,
       sourcemap: true,
       rollupOptions: {
         external: ['vue'],
         output: [
           {
             format: 'cjs',
-            entryFileNames: `vue-sonner.cjs`
+            assetFileNames: 'style.css'
           },
           {
             format: 'es',
-            entryFileNames: `vue-sonner.js`,
-            preserveModules: false
+            preserveModules: false,
+            assetFileNames: 'style.css'
           }
         ]
       }
@@ -62,42 +72,52 @@ export default defineConfig(({ command, mode }) => {
       ...commonPlugins,
       {
         name: 'inline-css',
+        buildStart() {
+          cssCodeStr = ''
+        },
         transform(code, id) {
           const isCSS = (path: string) => /\.css$/.test(path)
           if (!isCSS(id)) return
 
-          const cssCode = minify(code)
-          cssCodeStr = cssCode
+          const minifiedCss = minify(code)
+          cssCodeStr += minifiedCss
           return {
-            code: '',
+            code: minifiedCss,
             map: { mappings: '' }
           }
         },
-        renderChunk(code, { isEntry }) {
-          if (!isEntry) return
+        renderChunk(code, chunk) {
+          if (!chunk.isEntry) return null
 
+          if (chunk.name === 'vue-sonner') {
+            return {
+              code: `\
+              function __insertCSSVueSonner(code) {
+                if (!code || typeof document == 'undefined') return
+                
+                function insertCSS() {
+  
+                  let head = document.head || document.getElementsByTagName('head')[0]
+                  if (!head) return
+                  let style = document.createElement('style')
+                  style.type = 'text/css'
+                  head.appendChild(style)
+                  style.styleSheet ? (style.styleSheet.cssText = code) : style.appendChild(document.createTextNode(code))
+                }
+  
+                if (document.readyState === 'loading') {
+                  document.addEventListener('DOMContentLoaded', insertCSS)
+                } else {
+                  insertCSS()
+                }
+              }\n
+              __insertCSSVueSonner(${JSON.stringify(cssCodeStr)})
+              \n ${code}`,
+              map: { mappings: '' }
+            }
+          }
           return {
-            code: `\
-            function __insertCSSVueSonner(code) {
-              if (!code || typeof document == 'undefined') return
-              
-              function insertCSS() {
-                let head = document.head || document.getElementsByTagName('head')[0]
-                if (!head) return
-                let style = document.createElement('style')
-                style.type = 'text/css'
-                head.appendChild(style)
-                style.styleSheet ? (style.styleSheet.cssText = code) : style.appendChild(document.createTextNode(code))
-              }
-
-              if (document.readyState === 'loading') {
-                document.addEventListener('DOMContentLoaded', insertCSS)
-              } else {
-                insertCSS()
-              }
-            }\n
-            __insertCSSVueSonner(${JSON.stringify(cssCodeStr)})
-            \n ${code}`,
+            code,
             map: { mappings: '' }
           }
         }
